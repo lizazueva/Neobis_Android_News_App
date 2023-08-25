53acd518b49fecc0da3ed91e3366f177c99960c1
Елизавета Зуева <elizavetazueva@MacBook-Air-Elizaveta.local>
2023-08-25 21:47:39 +0300

    Fefactor code and xml

--- app/src/main/java/com/example/myapplication/fragments/NewsFragment.kt
+++ app/src/main/java/com/example/myapplication/fragments/NewsFragment.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -36,6 +37,7 @@ class NewsFragment : Fragment(){
         return binding.root
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModelNewsFragment = (activity as MainActivity).myViewModel
